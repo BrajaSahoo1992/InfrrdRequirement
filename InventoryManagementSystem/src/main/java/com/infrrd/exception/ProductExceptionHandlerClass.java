8085712d54c55a529eb85833a795f59c9ddd7347
BRAJA <BRAJA@DESKTOP-1B580C2>
2020-03-30 17:21:38 +0530

    commit

--- InventoryManagementSystem/src/main/java/com/infrrd/exception/ProductExceptionHandlerClass.java
+++ InventoryManagementSystem/src/main/java/com/infrrd/exception/ProductExceptionHandlerClass.java
@@ -1,7 +1,11 @@
 package com.infrrd.exception;
 
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,6 +13,15 @@ import com.infrrd.entity.ErrorMessage;
 
 @RestControllerAdvice
 public class ProductExceptionHandlerClass {
+	//this is for validation error 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorMessage> handleGlobalExceptions(MethodArgumentNotValidException ex) {
+		String response=ex.getBindingResult().getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.joining(","));
+		ErrorMessage errorMessage=new ErrorMessage();
+		errorMessage.setErrorCode(HttpStatus.BAD_REQUEST.value());
+		errorMessage.setErrorMessage(response);
+		return new ResponseEntity<ErrorMessage>(errorMessage,HttpStatus.BAD_REQUEST);
+    }
 	
 	@ExceptionHandler(Exception.class)
 	public String allException(){
